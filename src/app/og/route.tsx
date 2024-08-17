700dc497a3ff9a2571b22a06600a00c074f57ea7
Lorant <lorant.toth.94@gmail.com>
2024-08-17 23:04:31 +0200

    dynamic og for blog posts

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -1,22 +1,53 @@
 import { ImageResponse } from 'next/og'
+import { baseURL } from '@/app/resources';
 
 export const runtime = 'edge';
 
-export function GET(request: Request) {
+export async function GET(request: Request) {
     let url = new URL(request.url)
     let title = url.searchParams.get('title') || 'Portfolio'
+    const font = fetch(
+        new URL('../../../public/fonts/Inter.ttf', import.meta.url)
+    ).then((res) => res.arrayBuffer());
+    const fontData = await font;
 
     return new ImageResponse(
         (
-            <div style={{ display: 'flex', width: '100%', height: '100%', padding: '4rem' }}>
-                <h1 style={{ fontSize: '3rem', fontWeight: '800' }}>
-                    {title}
-                </h1>
+            <div
+                style={{
+                    display: 'flex',
+                    width: '100%',
+                    height: '100%',
+                    paddingTop: '12rem',
+                    backgroundImage: `url(https://${baseURL}/og/template.jpg)`
+                }}>
+                <div
+                    style={{
+                        marginLeft: 190,
+                        marginRight: 190,
+                        display: 'flex',
+                        fontSize: 130,
+                        fontFamily: 'Inter',
+                        letterSpacing: '-0.05em',
+                        fontStyle: 'normal',
+                        color: 'white',
+                        lineHeight: '120px',
+                        whiteSpace: 'pre-wrap',
+                    }}>
+                {title}
+                </div>
             </div>
         ),
         {
             width: 1920,
             height: 1080,
+            fonts: [
+                {
+                    name: 'Inter',
+                    data: fontData,
+                    style: 'normal',
+                },
+              ],
         }
     )
 }
