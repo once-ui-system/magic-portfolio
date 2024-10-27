51061506fddff96ae5750320f3528fadd77dc219
Lorant <lorant@once-ui.com>
2024-10-27 20:44:45 +0000

    fix header alignment on desktop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -78,88 +78,94 @@ export const Header = () => {
             fillWidth padding="8"
             justifyContent="center">
             <Flex
-                hide="s"
                 paddingLeft="12" fillWidth
                 alignItems="center"
                 textVariant="body-default-s">
                 { display.location && (
-                    <>{person.location}</>
+                    <Flex hide="s">
+                        {person.location}
+                    </Flex>
                 )}
             </Flex>
-            <Flex
-                background="surface" border="neutral-medium" borderStyle="solid-1" radius="m-4" shadow="l"
-                padding="4"
-                justifyContent="center">
+            <Flex fillWidth justifyContent="center">
                 <Flex
-                    gap="4"
-                    textVariant="body-default-s">
-                    { routes['/'] && (
-                        <ToggleButton
-                            prefixIcon="home"
-                            href={`/${params?.locale}`}
-                            selected={pathname === "/"}>
-                            <Flex paddingX="2" hide="s">{home.label}</Flex>
-                        </ToggleButton>
-                    )}
-                    { routes['/about'] && (
-                        <ToggleButton
-                            prefixIcon="person"
-                            href={`/${params?.locale}/about`}
-                            selected={pathname === "/about"}>
-                            <Flex paddingX="2" hide="s">{about.label}</Flex>
-                        </ToggleButton>
-                    )}
-                    { routes['/work'] && (
-                        <ToggleButton
-                            prefixIcon="grid"
-                            href={`/${params?.locale}/work`}
-                            selected={pathname.startsWith('/work')}>
-                            <Flex paddingX="2" hide="s">{work.label}</Flex>
-                        </ToggleButton>
-                    )}
-                    { routes['/blog'] && (
-                        <ToggleButton
-                            prefixIcon="book"
-                            href={`/${params?.locale}/blog`}
-                            selected={pathname.startsWith('/blog')}>
-                            <Flex paddingX="2" hide="s">{blog.label}</Flex>
-                        </ToggleButton>
-                    )}
-                    { routes['/gallery'] && (
-                        <ToggleButton
-                            prefixIcon="gallery"
-                            href={`/${params?.locale}/gallery`}
-                            selected={pathname.startsWith('/gallery')}>
-                            <Flex paddingX="2" hide="s">{gallery.label}</Flex>
-                        </ToggleButton>
-                    )}
+                    background="surface" border="neutral-medium" borderStyle="solid-1" radius="m-4" shadow="l"
+                    padding="4"
+                    justifyContent="center">
+                    <Flex
+                        gap="4"
+                        textVariant="body-default-s">
+                        { routes['/'] && (
+                            <ToggleButton
+                                prefixIcon="home"
+                                href={`/${params?.locale}`}
+                                selected={pathname === "/"}>
+                                <Flex paddingX="2" hide="s">{home.label}</Flex>
+                            </ToggleButton>
+                        )}
+                        { routes['/about'] && (
+                            <ToggleButton
+                                prefixIcon="person"
+                                href={`/${params?.locale}/about`}
+                                selected={pathname === "/about"}>
+                                <Flex paddingX="2" hide="s">{about.label}</Flex>
+                            </ToggleButton>
+                        )}
+                        { routes['/work'] && (
+                            <ToggleButton
+                                prefixIcon="grid"
+                                href={`/${params?.locale}/work`}
+                                selected={pathname.startsWith('/work')}>
+                                <Flex paddingX="2" hide="s">{work.label}</Flex>
+                            </ToggleButton>
+                        )}
+                        { routes['/blog'] && (
+                            <ToggleButton
+                                prefixIcon="book"
+                                href={`/${params?.locale}/blog`}
+                                selected={pathname.startsWith('/blog')}>
+                                <Flex paddingX="2" hide="s">{blog.label}</Flex>
+                            </ToggleButton>
+                        )}
+                        { routes['/gallery'] && (
+                            <ToggleButton
+                                prefixIcon="gallery"
+                                href={`/${params?.locale}/gallery`}
+                                selected={pathname.startsWith('/gallery')}>
+                                <Flex paddingX="2" hide="s">{gallery.label}</Flex>
+                            </ToggleButton>
+                        )}
+                    </Flex>
                 </Flex>
             </Flex>
-            <Flex
-                paddingRight="12"
-                justifyContent="flex-end" alignItems="center"
-                textVariant="body-default-s"
-                gap="20">
-                {routing.locales.length > 1 && <Flex
-                    background="surface" border="neutral-medium" borderStyle="solid-1" radius="m-4" shadow="l"
-                    padding="4" gap="2"
-                    justifyContent="center">
-                        {i18n && routing.locales.map((locale, index) => (
-                            <ToggleButton
-                                key={index}
-                                selected={params?.locale === locale}
-                                onClick={() => handleLanguageChange(locale)}
-                                className={isPending && 'pointer-events-none opacity-60' || ''}
-                                >
-                                {locale.toUpperCase()}
-                            </ToggleButton>
-                        ))}
+            <Flex fillWidth justifyContent="flex-end" alignItems="center">
+                <Flex
+                    paddingRight="12"
+                    justifyContent="flex-end" alignItems="center"
+                    textVariant="body-default-s"
+                    gap="20">
+                    {routing.locales.length > 1 &&
+                        <Flex
+                            background="surface" border="neutral-medium" borderStyle="solid-1" radius="m-4" shadow="l"
+                            padding="4" gap="2"
+                            justifyContent="center">
+                            {i18n && routing.locales.map((locale, index) => (
+                                <ToggleButton
+                                    key={index}
+                                    selected={params?.locale === locale}
+                                    onClick={() => handleLanguageChange(locale)}
+                                    className={isPending && 'pointer-events-none opacity-60' || ''}
+                                    >
+                                    {locale.toUpperCase()}
+                                </ToggleButton>
+                            ))}
+                        </Flex>
+                    }
+                    <Flex hide="s">
+                        { display.time && (
+                            <TimeDisplay timeZone={person.location}/>
+                        )}
                     </Flex>
-                }
-                <Flex hide="s">
-                    { display.time && (
-                        <TimeDisplay timeZone={person.location}/>
-                    )}
                 </Flex>
             </Flex>
         </Flex>
