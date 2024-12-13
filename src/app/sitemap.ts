ae6da0dcb9ad8f541205456dd8f6bb4ec8f6b889
Zsofia <zsofia.komaromi@gmail.com>
2024-12-13 11:44:55 +0100

    Don't include locale prefix if there is only one locale
    This aligns the generated sitemap structure with how we handle locale in routing: only include the locale prefix if there is more than one locale set.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,28 +3,28 @@ import { baseURL, routes as routesConfig } from '@/app/resources'
 import { routing } from '@/i18n/routing'
 
 export default async function sitemap() {
-
     const locales = routing.locales;
+    const includeLocalePrefix = locales.length > 1;
 
     let blogs = locales.flatMap((locale) => 
         getPosts(['src', 'app', '[locale]', 'blog', 'posts', locale]).map((post) => ({
-            url: `${baseURL}/${locale}/blog/${post.slug}`,
+            url: `${baseURL}${includeLocalePrefix ? `/${locale}` : ''}/blog/${post.slug}`,
             lastModified: post.metadata.publishedAt,
         }))
     );
 
     let works = locales.flatMap((locale) => 
         getPosts(['src', 'app', '[locale]', 'work', 'projects', locale]).map((post) => ({
-            url: `${baseURL}/${locale}/work/${post.slug}`,
+            url: `${baseURL}${includeLocalePrefix ? `/${locale}` : ''}/work/${post.slug}`,
             lastModified: post.metadata.publishedAt,
         }))
     );
 
-    const activeRoutes  = Object.keys(routesConfig).filter((route) => routesConfig[route]);
+    const activeRoutes = Object.keys(routesConfig).filter((route) => routesConfig[route]);
 
     let routes = locales.flatMap((locale)=> 
         activeRoutes.map((route) => ({
-            url: `${baseURL}/${locale}${route !== '/' ? route : ''}`,
+            url: `${baseURL}${includeLocalePrefix ? `/${locale}` : ''}${route !== '/' ? route : ''}`,
             lastModified: new Date().toISOString().split('T')[0],
         }))
     );
