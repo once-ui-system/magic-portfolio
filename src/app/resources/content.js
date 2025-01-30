a66619ec578be7bb9c763fddad8f1e22a3a6321b
Mohamed Said <moasaid9@gmail.com>
2025-01-30 21:39:22 +0000

    update-one

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,8 +1,8 @@
 import { InlineCode } from "@/once-ui/components";
 
 const person = {
-  firstName: "Selene",
-  lastName: "Yu",
+  firstName: "Mo",
+  lastName: "Said",
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
