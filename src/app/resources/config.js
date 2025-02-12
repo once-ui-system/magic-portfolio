1ce4d76c8d3a9f54a3e2a95e7dfaa5a61e7fb38c
arsh <96003154+arshxb@users.noreply.github.com>
2025-02-12 04:35:16 -0600

    Move password to .env file for better security and easier management (#47)
    * Move password to .env file for better security
    
    * ui: update input on password protected pages
    
    ---------
    
    Co-authored-by: Lorant <lorant@once-ui.com>
    Co-authored-by: Lorant <34750239+lorant-one@users.noreply.github.com>

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -9,7 +9,7 @@ const routes = {
 };
 
 // Enable password protection on selected routes
-// Set password in pages/api/authenticate.ts
+// Set password in the .env file, refer to .env.example
 const protectedRoutes = {
   "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
 };
