589fc0e771bc6830ff611491fe798d29630d9f0d
Félix Bouazza <fbouazza.dev@gmail.com>
2025-02-12 11:38:47 +0100

    fix: add "https://" to sitemap urls (#50)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import { baseURL, routes as routesConfig } from "@/app/resources";
 
 export default async function sitemap() {
   const blogs = getPosts(["src", "app", "blog", "posts"]).map((post) => ({
-    url: `${baseURL}/blog/${post.slug}`,
+    url: `https://${baseURL}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }));
 
   const works = getPosts(["src", "app", "work", "projects"]).map((post) => ({
-    url: `${baseURL}/work/${post.slug}`,
+    url: `https://${baseURL}/work/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }));
 
   const activeRoutes = Object.keys(routesConfig).filter((route) => routesConfig[route]);
 
   const routes = activeRoutes.map((route) => ({
-    url: `${baseURL}${route !== "/" ? route : ""}`,
+    url: `https://${baseURL}${route !== "/" ? route : ""}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
