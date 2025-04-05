797ba36c32bda4fcc8feaa0a0255ce2586b55d28
Akshay Sabale <akshaysabale07169@gmail.com>
2025-04-05 17:14:20 -0500

    streamlined changes

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -10,7 +10,7 @@ const person = {
   role: "Software Engineer",
   avatar: "/images/avatar.png",
   location: "America/Chicago", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Gujarati", "Hindi"], // optional: Leave the array empty if you don't want to display languages
+  languages: [ ], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter = {
@@ -68,7 +68,7 @@ const home = {
 const about = {
   label: "About",
   title: "About me",
-  description: `Meet ${person.name}, ${person.role} from ${person.location}`,
+  description: `Meet ${person.name}, ${person.role} from Texas, USA`,
   tableOfContent: {
     display: true,
     subItems: false,
@@ -77,7 +77,7 @@ const about = {
     display: true,
   },
   calendar: {
-    display: true,
+    display: false,
     link: "https://cal.com",
   },
   intro: {
@@ -232,12 +232,12 @@ const gallery = {
     {
       src: "/images/gallery/img-01.jpg",
       alt: "image",
-      orientation: "vertical",
+      orientation: "horizontal",
     },
     {
       src: "/images/gallery/img-02.jpg",
       alt: "image",
-      orientation: "horizontal",
+      orientation: "vertical",
     },
   ],
 };
