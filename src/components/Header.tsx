46bf526ebd43d2a2304869ee452a1c28a04ca329
Lorant <lorant@once-ui.com>
2025-04-23 12:20:06 +0200

    fix: make header sticky on desktop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -52,7 +52,7 @@ export const Header = () => {
       <Fade show="s" fillWidth position="fixed" bottom="0" to="top" height="80" zIndex={9} />
       <Flex
         fitHeight
-        position={undefined}
+        position="sticky"
         className={styles.position}
         as="header"
         zIndex={9}
