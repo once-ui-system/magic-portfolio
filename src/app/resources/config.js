9a0c78b819ce2320a2c66113394391fa108ee922
Lorant <lorant@once-ui.com>
2025-04-23 11:17:46 +0200

    update config to prevent build error

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -38,6 +38,7 @@ const font = {
 };
 
 const style = {
+  theme: "dark", // theme is not necessary when using ThemeProvider (default)
   neutral: "gray", // sand | gray | slate
   brand: "cyan", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
   accent: "red", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
