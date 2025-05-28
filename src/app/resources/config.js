d96416af96a9dcd4cdcd98cbdaa6453b9115a0c2
Marcel Kuhn <kontakt@marcel-kuhn.de>
2025-05-28 14:56:08 +0000

    update: projects

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -15,7 +15,7 @@ const routes = {
 // Enable password protection on selected routes
 // Set password in the .env file, refer to .env.example
 const protectedRoutes = {
-  "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
+  // "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
 };
 
 import { Geist } from "next/font/google";
