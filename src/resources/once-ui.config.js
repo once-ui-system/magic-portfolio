7693fd8e922bb0f70b28c0924e81733d4458197e
Lorant <lorant@once-ui.com>
2025-06-10 22:22:02 +0200

    fix: change baseURL in config

--- src/resources/once-ui.config.js
+++ src/resources/once-ui.config.js
@@ -1,5 +1,5 @@
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL = "https://demo.once-ui.com";
+const baseURL = "https://demo.magic-portfolio.com";
 
 const routes = {
   "/": true,
