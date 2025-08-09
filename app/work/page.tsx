356c1d2cde577adb2555c105d91238db7ed38961
Awais <awaissmr@gmail.com>
2025-08-09 20:23:20 +0500

    Add GitHub activity calendar to Work page
    Integrates the react-github-calendar component to display a GitHub activity calendar for the user 'Awii21' on the Work page. Updates dependencies to include react-github-calendar.

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,7 @@
 import { Column, Meta, Schema } from "@once-ui-system/core";
 import { baseURL, about, person, work } from "@/resources";
 import { Projects } from "@/components/work/Projects";
-
+import GitHubCalendar from "react-github-calendar";
 export async function generateMetadata() {
   return Meta.generate({
     title: work.title,
@@ -15,6 +15,30 @@ export async function generateMetadata() {
 export default function Work() {
   return (
     <Column maxWidth="m">
+      <h1
+        style={{
+          paddingBottom: "2rem",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        GitHub Activity Calendar
+      </h1>
+      <div
+        style={{
+          paddingBottom: "5rem",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <GitHubCalendar
+          username="Awii21"
+          blockSize={13}
+          blockMargin={5}
+          fontSize={18}
+        />
+      </div>
+
       <Schema
         as="webPage"
         baseURL={baseURL}
