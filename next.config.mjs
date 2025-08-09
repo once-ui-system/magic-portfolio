736d4f6d563fccdded340c4a62d98c69e21b7b55
Awais <awaissmr@gmail.com>
2025-08-09 19:35:40 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,23 +2,17 @@ import mdx from "@next/mdx";
 
 const withMDX = mdx({
   extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
+  options: {},
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
-  experimental: {
-    mdxRs: true,
-  },
   sassOptions: {
+    compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
   },
-  
 };
 
 export default withMDX(nextConfig);
