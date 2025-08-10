d3dbd6cfb988e7f2029dad37851f6d3fec24151b
Awais <awaissmr@gmail.com>
2025-08-10 22:08:19 +0500

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 import { Column, Heading, Meta, Schema } from "@once-ui-system/core";
 import { Mailchimp } from "@/components";
 import { Posts } from "@/components/blog/Posts";
-import { baseURL, blog, person, newsletter } from "@/resources";
+import { baseURL, blog, person, emailForm } from "@/resources";
 
 export async function generateMetadata() {
   return Meta.generate({
@@ -38,7 +38,7 @@ export default function Blog() {
 				<Posts range={[2,3]} thumbnail/>
 				<Posts range={[4]} columns="2"/>
 			</Column>
-      {newsletter.display && <Mailchimp newsletter={newsletter} />}
+      {emailForm.display && <Mailchimp emailForm={emailForm} />}
     </Column>
   );
 }
