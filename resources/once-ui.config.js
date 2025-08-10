20feacbdc96571aa8bf55d0f514faa620472f284
Awais <awaissmr@gmail.com>
2025-08-10 22:51:06 +0500

    Update once-ui.config.js theme color
    Changed the primary color in resources/once-ui.config.js from '#1e40af' to '#0d9488' to update the application's theme. No other configuration changes were made.

--- resources/once-ui.config.js
+++ resources/once-ui.config.js
@@ -1,7 +1,7 @@
 import { home } from "./content";
 
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL = "https://demo.magic-portfolio.com";
+const baseURL = "https://awaissoomro.dev";
 
 const routes = {
   "/": true,
@@ -182,9 +182,8 @@ const schema = {
 
 // social links
 const sameAs = {
-  threads: "https://www.threads.com/@once_ui",
-  linkedin: "https://www.linkedin.com/company/once-ui/",
-  discord: "https://discord.com/invite/5EyAQ4eNdS",
+  threads: "https://www.threads.com/@awais_soomro_",
+  linkedin: "https://linkedin.com/in/awais-8458111b3",
 };
 
 export { display, mailchimp, routes, protectedRoutes, baseURL, fonts, style, schema, sameAs, effects, dataStyle };
