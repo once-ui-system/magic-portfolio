20b36284793982f1b1bfe89d2eee9b4642a74af6
Awais <awaissmr@gmail.com>
2025-08-12 02:08:08 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,11 @@ const nextConfig = {
     compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
   },
+   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withMDX(nextConfig);
