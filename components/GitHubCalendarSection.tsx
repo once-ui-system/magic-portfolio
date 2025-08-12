383618d99787857143ff08c2450a7acee4271ff2
Awais <awaissmr@gmail.com>
2025-08-12 06:39:28 +0500

    Update GitHubCalendarSection.tsx

--- components/GitHubCalendarSection.tsx
+++ components/GitHubCalendarSection.tsx
@@ -5,26 +5,11 @@ import GitHubCalendar from "react-github-calendar";
 
 export default function GitHubCalendarSection() {
   return (
-    <section style={{ marginBottom: "4rem" }}>
-      <h1
-        style={{
-          paddingBottom: "2rem",
-          display: "flex",
-          justifyContent: "center",
-          fontSize: "2.5rem",
-          fontWeight: "bold",
-          margin: 0,
-        }}
-      >
+    <section className="mb-16">
+      <h1 className="mb-32 flex justify-center text-2xl sm:text-lg md:text-xl lg:text-2xl font-bold m-0">
         GitHub Activity Calendar
       </h1>
-      <div
-        style={{
-          paddingBottom: "2rem",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
+      <div className="pb-8 flex justify-center">
         <GitHubCalendar username="Awii21" blockSize={13} blockMargin={5} fontSize={18} />
       </div>
     </section>
