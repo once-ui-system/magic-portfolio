03983c884b95f42204bb4ff6b938082fd86ab183
Love Khatri <83165022+LoveKhatri@users.noreply.github.com>
2025-08-17 23:26:03 +0530

    add types to content and config files (#122)
    * feat: add tzdata dependency and update content types for person and newsletter
    
    * feat: add configuration types for Once UI settings and styles
    
    * feat: update BasePageConfig to include image path for OG image
    
    * fix: typescript errors
    
    ---------
    
    Co-authored-by: Lorant <lorant@once-ui.com>

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -87,20 +87,20 @@ export default async function Blog({
           </Column>
           <ScrollToHash />
         </Column>
-    </Row>
-    <Column maxWidth={12} paddingLeft="40" fitHeight position="sticky" top="80" gap="16" hide="m">
-      <Row
-        gap="12"
-        paddingLeft="2"
-        vertical="center"
-        onBackground="neutral-medium"
-        textVariant="label-default-s"
-      >
-        <Icon name="document" size="xs" />
-        On this page
       </Row>
-      <HeadingNav fitHeight/>
-    </Column>
+      <Column maxWidth={12} paddingLeft="40" fitHeight position="sticky" top="80" gap="16" hide="m">
+        <Row
+          gap="12"
+          paddingLeft="2"
+          vertical="center"
+          onBackground="neutral-medium"
+          textVariant="label-default-s"
+        >
+          <Icon name="document" size="xs" />
+          On this page
+        </Row>
+        <HeadingNav fitHeight/>
+      </Column>
     </Row>
   );
 }
