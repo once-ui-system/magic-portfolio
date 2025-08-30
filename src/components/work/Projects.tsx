5296a5e2938d1f9eb1a38850a99c9912df5f7a27
Lorant <lorant@once-ui.com>
2025-08-30 20:33:43 +0200

    fix: broken work URL in related projects

--- src/components/work/Projects.tsx
+++ src/components/work/Projects.tsx
@@ -29,7 +29,7 @@ export function Projects({ range, exclude }: ProjectsProps) {
         <ProjectCard
           priority={index < 2}
           key={post.slug}
-          href={`work/${post.slug}`}
+          href={`/work/${post.slug}`}
           images={post.metadata.images}
           title={post.metadata.title}
           description={post.metadata.summary}
